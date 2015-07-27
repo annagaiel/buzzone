092f5518c84a3bec14430d67899ab20cac39047f
vagrant <annagaiel@gmail.com>
2015-07-27 12:18:14 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buzzone::Application.config.secret_key_base = '442d4e64af859f6820de430d6c7b48a4769e7650c47f774f4beeab7a6a6a6389d1fad60b065e2fa3f953b7c39035151ce65b130ea8a16819c98bb2fe90ddeb90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Buzzone::Application.config.session_store :cookie_store, key: '_buzzone_session'
