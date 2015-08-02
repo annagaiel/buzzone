71f32bf74b00fe0df001fa4bfb003e6b7aacd593
Anna Natorilla <annagaiel@gmail.com>
2015-08-02 09:25:03 +0000

    add comment form on show page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,7 +21,8 @@ class PlacesController < ApplicationController
 
 	def show
 		@place = Place.find(params[:id])
-  	end
+  	@comment = Comment.new
+    end
 
   	def edit
   		@place = Place.find(params[:id])
